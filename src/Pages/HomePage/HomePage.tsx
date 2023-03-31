2aa80d4fe50d785312efb8cf65070dba40c0f9cc
Simon <simon-menard@etu.univ-lyon1.fr>
2023-03-31 15:12:25 +0200

    fixed logo being to big, switched to module styling

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,4 @@
+import styles from './HomePage.module.css'
+export default function HomePage(){
+    return(<div>Home Page</div>);
+}
---
+++ src/Pages/RoomsPage/RoomsPage.tsx
@@ -0,0 +1,7 @@
+import styles from './RoomsPage.module.css';
+
+export default function RoomsPage(){
+    return (
+        <div>Room Page</div>
+        )
+}
