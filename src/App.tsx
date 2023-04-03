7ecea192a2e658b59d0bde40688c7318bbdd1e48
Simon <simon-menard@etu.univ-lyon1.fr>
2023-04-03 10:23:55 +0200

    modifed css module for HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
-import HomePage from './components/Pages/HomePage';
-import RoomPage from './components/Pages/RoomPage';
 import Router from './components/Router/Router';
 
 function App() {
-  const PAGE = window.location.href.split('/').pop();
-  console.log(PAGE);
   return (
     <>
       <Router></Router>
