e3ed6abf9cef5300f31563b2061e4b30dd2f1db9
elliot-cx <derfire22@gmail.com>
2023-04-03 17:39:17 +0200

    add docstring

--- app/services/botAuthentification.ts
+++ app/services/botAuthentification.ts
@@ -1,3 +1,8 @@
+/**
+ * This module contains functions related to authentication for a swarm bot.
+ *
+ * @packageDocumentation
+ */
 import { PORT } from "../..";
 import { Authentification } from "../models/authentification";
 
@@ -8,13 +13,18 @@ var auth: Authentification = {
     expiration: 1680555557560
 }
 
-// Coté client
+// Front side
 const discordClientId = 688126093424721954n;
 const uri = `localhost:${PORT}/auth`;
 const url = `https://discord.com/api/oauth2/authorize?client_id=${discordClientId}&redirect_uri=${encodeURIComponent(uri)}&response_type=token&scope=identify`
 
 export namespace BotAuthentificationService{
 
+    /**
+     * Gets the current authentication information for the bot.
+     *
+     * @returns The current authentication information or null if it has expired.
+     */
     export function getAuthentification() {
         // Check expiration
         if (new Date() > new Date(auth.expiration * 1000)) {
@@ -25,6 +35,11 @@ export namespace BotAuthentificationService{
         }
     }
 
+    /**
+     * Sets the authentication information for the bot.
+     *
+     * @param authentification - The new authentication information for the bot.
+     */
     export function setAuthentification(authentification: Authentification) {
         auth = authentification;
     }
--- app/services/sockets.ts
+++ app/services/sockets.ts
@@ -1,39 +1,47 @@
+/**
+ * This module contains functions related to sockets for a swarm bot.
+ *
+ * @packageDocumentation
+ */
 import { Server } from 'http';
 import { Socket } from 'socket.io';
 import { RoomServices } from './room';
 
-export namespace SocketIOService{
+export namespace SocketIOService {
 
     var io = require('socket.io');
 
+    /**
+     * Initializes the socket.io server on the provided HTTP server and listens for socket connections.
+     * @param server The HTTP server to use for socket.io.
+     */
     export function initSockets(server: Server) {
-        // Initialisation du serveur
         io = io(server);
-        // Connexion du client React
         io.on('connection', (socket: Socket) => {
-    
-            // Le client veut écouter les MAJ des bots
-            socket.on('join', (roomCode: string,  callback: Function) => {
+            socket.on('join', (roomCode: string, callback: Function) => {
                 const room = RoomServices.getRoomByID(roomCode);
                 if (room) {
                     socket.join(room.id);
                 }
             });
-    
-            // Déconnexion
-            socket.on('disconnect', () => {});
-    
+            socket.on('disconnect', () => { });
         });
     }
 
+    /**
+     * Emits the specified event with the provided arguments to all clients in the given room.
+     * @param roomCode The ID of the room to emit the event to.
+     * @param eventName The name of the event to emit.
+     * @param args Any arguments to pass with the event.
+     */
     export function emitRoom(roomCode: string, eventName: string, ...args: any[]) {
         const room = RoomServices.getRoomByID(roomCode);
         if (!room) {
-          console.error(`Room ${roomCode} does not exist.`);
-          return;
+            console.error(`Room ${roomCode} does not exist.`);
+            return;
         }
         io.to(room.id).emit(eventName, ...args);
     }
-    
+
 }
 
