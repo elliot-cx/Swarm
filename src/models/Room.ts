00c5eeed9135f21d2baf09f44f97372c37b42eaa
Simon <simon-menard@etu.univ-lyon1.fr>
2023-04-05 12:49:05 +0200

    created Bot.ts & Room.ts

---
+++ src/models/Room.ts
@@ -0,0 +1,10 @@
+import Bot from  './Bot/Bot';
+export default interface Room{
+    id: string,
+    name: string,
+    type: string,
+    nbPlayers: number,
+    isActive: boolean,
+    link: string,
+    bots: Bot[];
+}
