2d935b7bed8665f52443c626fa22c7a1c281989b
Simon <simon-menard@etu.univ-lyon1.fr>
2023-04-05 12:46:53 +0200

    modif home page

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -8,22 +8,22 @@ export default function HomePage(){
     const tutorial = () => {
         return(
             <div className={styles.tutorial}>
-                <h2>Pour commencer</h2>
-                <p>Depuis la section de droite ou depuis la page rooms, ajouter des rooms en renseignant un nom et le code fourni par JKLM</p>
+                <h2>Pour <b>commencer</b></h2>
+                <p>Depuis la section de droite ou depuis la page rooms, <b>ajouter des rooms</b> en renseignant un nom et le code fourni par JKLM</p>
                 <p>Attention, cette application de crée pas de parties sur JKLM. Par rooms, nous entendons les parties gérées par Swarm.</p>
                 <h3>Ajouter des bots</h3>
-                <p>Après avoir créé une rooms, vous pourrez la selectionner et l'administrer.</p>
-                <p>Ajoutez jusqu'à 16 bots du type de votre choix et envoyez la sauce !</p>
+                <p>Après avoir créé une room, vous pourrez la selectionner et l'administrer.</p>
+                <p>Ajoutez jusqu'à 16 bots du type de votre choix et <b>envoyez la sauce</b> !</p>
                 <h4>Etat des bots et contrôle de leurs actions</h4>
-                <p>Swarm vous permet d'afficher en temps réel l'état de chaque bot (connecte, banni, déconnecté etc..)</p>
+                <p>Swarm affiche en temps réel <b>l'état de chaque bot</b> (connecte, banni, déconnecté etc..)</p>
             </div>
         )
     }
     return(
     <main id={styles.homeRoot}>
         <Section 
-            title1='Bienvenue sur SwarmBot' 
-            title2= {<h2>Votre outil de <b className={styles.titleAccent}>spam, troll et cheat</b> sur JKLM</h2>} 
+            title1='Bienvenue sur SwarmBot'
+            title2= {<h2>Votre outil de <b>spam, troll et cheat</b> sur JKLM</h2>} 
             text="Cette application vous permet de contrôler jusqu'à 16 bots de différents types : spam, cheat et bientôt tracker."
             contentChildrenNodes={[
                 tutorial()
