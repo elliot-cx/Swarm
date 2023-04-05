63d3b6a82fedfb10a03a0b1afaaaf0b309f01550
Simon <simon-menard@etu.univ-lyon1.fr>
2023-04-05 12:47:39 +0200

    modif router

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,15 @@ export default function Header(){
     const logoClick = (e:any)=>{
         window.location.href='./'
     }
+    const navButtonClick = (e:Event) =>{
+        let target = e.target as any;
+        if(target) target.classList.add('clicked');
+    }
+
     return(
         <header id={styles.headerRoot}>    
-            <Link className={styles.logo} to='./'><img onClick={logoClick} src ='./assets/images/Logo.png'></img></Link>
-            <Link to='./rooms'>Rooms</Link>
+            <Link className={styles.logo} to='./'><img alt="Swarm Bot"onClick={logoClick} src ='./assets/images/Logo.png'></img></Link>
+            <Link className={styles.navButton} to='./rooms'>Rooms</Link>
         </header>
     );
 }
