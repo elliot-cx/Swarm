9d03eb9da0bb4615b6856573e17d0c74aae9ac20
Simon <simon-menard@etu.univ-lyon1.fr>
2023-04-06 19:03:44 +0200

    added HttpUtils namespace

--- src/utils/HttpUtils.ts
+++ src/utils/HttpUtils.ts
@@ -1,7 +1,9 @@
-export const API_URL = 'http://localhost:8080'
+export const API_URL = 'http://localhost:6969'
 
 const axios = require('axios');
+export namespace HttpUtils {   
+    export const fetchData = <T>(url:string): Promise<T> => {
+        return fetch(url).then((res) => res.json());
+    }
 
-export const fetchData = <T>(url:string): Promise<T> => {
-    return fetch(url).then((res) => res.json());
 }
