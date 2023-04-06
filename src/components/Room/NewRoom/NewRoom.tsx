d99ced41982f8a2702b1196ad5e733d16440725f
Simon <simon-menard@etu.univ-lyon1.fr>
2023-04-06 17:45:07 +0200

    created postRoomData function

--- src/components/Room/NewRoom/NewRoom.tsx
+++ src/components/Room/NewRoom/NewRoom.tsx
@@ -1,18 +1,28 @@
 import { MouseEventHandler } from "react";
 import styles from "./NewRoom.module.css";
+import {Room} from "../../../models/Room";
+import { fetchData } from "../../../utils/HttpUtils";
 export default function NewRoom(){
-    const sendRoomData = (e:Event) => {
-        e.preventDefault();
-        console.log(1);
-        const roomCode = (document.getElementById("roomCodeField") as HTMLInputElement).value;
-        const headers = { "Content-Type": "application/json" };
-        let roomData:any;
-        console.log(roomData);
-    }   
+    const postRoomData = (submit:any) => {
+        submit.preventDefault();
+        let roomCode:string = submit.target[0].value as string;
+        let rooms:Room[]|undefined;
+        fetch('localhost:8080/room?provider=JKLM')
+        .then((res)=>res.json())
+        .then((data)=>rooms = data as Room[])
+        const room = rooms ? rooms.filter((obj)=>obj.id===roomCode) : undefined;
+        if(room){
+            fetchData<Room[]|{success:boolean}>('/room')
+            .then(console.log)
+        }else{
+            console.log('Error when fetching JKLM rooms.')
+        }
+    }
+
     return(
-        <div id={styles.newRoomRoot}>
-            <input id="roomCodeField" type='text' name="roomName" placeholder="Code de la room"></input>
-            <input id={styles.formSubmit}type="submit" onClick={sendRoomData as any as MouseEventHandler<HTMLInputElement>}></input>
-        </div>
+        <form onSubmit={postRoomData} id={styles.newRoomRoot}>
+            <input id="roomCodeField" type='text' name="roomCode" placeholder="Code de la room"></input>
+            <input id={styles.formSubmit}type="submit"></input>
+        </form>
     )
 }
