77fe74e2040d2e159702a8bb69fac1ed4aafee6c
Simon <simon-menard@etu.univ-lyon1.fr>
2023-04-08 11:06:29 +0200

    modif fetchData function

--- src/utils/HttpUtils.ts
+++ src/utils/HttpUtils.ts
@@ -2,8 +2,9 @@ export const API_URL = 'http://localhost:6969'
 
 const axios = require('axios');
 export namespace HttpUtils {   
-    export const fetchData = <T>(url:string): Promise<T> => {
-        return fetch(url).then((res) => res.json());
+    export const fetchData = <T>(apiRoute:string,params?:{[key:string]:any}): Promise<T> => {
+        return fetch(`${API_URL}/${apiRoute}`,params ? params : {})
+        .then((response) => response.json())
     }
 
 }
