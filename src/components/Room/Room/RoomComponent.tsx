6f5ee76ed565b0c365d6e562fd0d458595d601d4
Gerard2par2 <floogles71@gmail.com>
2023-04-09 16:22:14 +0200

    modif room comp

--- src/components/Room/Room/RoomComponent.tsx
+++ src/components/Room/Room/RoomComponent.tsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import {Room} from '../../../models/Room';
 import styles from './RoomComponent.module.css'
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 const MAX_BOT = 16;
 type Props={
     room:Room
@@ -13,15 +15,17 @@ export default function RoomComponent ({room}:Props){
             <div className={styles.columnContainer}>
                 <h1>{displayedRoom.id}</h1>
                 <h2>{displayedRoom.name}</h2>
-                <div>
+                <div className={styles.roomInfo}>
                     <p>{`Game: ${displayedRoom.type}`} </p>
                     <p>{`Players: ${displayedRoom.nbPlayers}`} </p>
                     <p>{`Bots: ${displayedRoom.bots.length} / ${MAX_BOT}`}</p>
                 </div>
             </div>
             <div className={styles.columnContainer}>
-                <Link to='./rooms' className={styles.selectRoom}><FontAwesomeIcon></FontAwesomeIcon></Link>
-                <div className={displayedRoom.isActive ? styles.ativeIcon : styles.inactiveIcon}></div>
+                <Link to={`./room?id=${displayedRoom.id}`} className={styles.selectRoom}><FontAwesomeIcon size="2x" icon={faCheck}></FontAwesomeIcon></Link>
+                <div className={styles.activityIconContainer}>
+                    <div className={ `${styles.activityIcon} ${displayedRoom.isActive ? styles.ativeIcon : styles.inactiveIcon}`}></div>
+                </div>
             </div>
         </div>
     )
