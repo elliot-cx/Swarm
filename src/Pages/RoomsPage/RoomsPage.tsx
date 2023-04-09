3a005e68b7d00d706ef3d8daea8967c889142b7a
Gerard2par2 <floogles71@gmail.com>
2023-04-09 16:22:37 +0200

    modif rooms page

--- src/Pages/RoomsPage/RoomsPage.tsx
+++ src/Pages/RoomsPage/RoomsPage.tsx
@@ -1,6 +1,8 @@
 import styles from './RoomsPage.module.css';
 
 export default function RoomsPage(){
+    const roomId = new URLSearchParams().get("id");
+    console.log(roomId);
     return (
         <div>Room Page</div>
         )
