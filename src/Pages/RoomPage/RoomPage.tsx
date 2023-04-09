b90380e4ed6aaf17897c409bcc13c2542a0225d8
Gerard2par2 <floogles71@gmail.com>
2023-04-09 20:37:34 +0200

    modif room page

--- src/Pages/RoomPage/RoomPage.tsx
+++ src/Pages/RoomPage/RoomPage.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { HttpUtils } from '../../utils/HttpUtils';
 import styles from './RoomPage.module.css';
 import { Room } from '../../models/Room';
+import RoomComponent from '../../components/Room/Room/RoomComponent';
+import Section from '../../components/Section/Section';
+import { Bot } from '../../models/Bot/Bot';
 
 export default function RoomsPage(){
     const roomId = new URLSearchParams(window.location.search).get("id");
@@ -12,7 +15,23 @@ export default function RoomsPage(){
         let room = (rooms as Room[]).filter(room=>room.id==roomId)[0];
         setRoom(room)
     })
+    const botsPanel = () => (
+        <div className={styles.botsPanel}>
+            <div className='botList'>
+                {room?.bots.map((bot:Bot,key:number)=>{
+                    return <div>
+                        <h1>{bot.name}</h1>
+                        <p>{`Type: ${bot.type}`}</p>
+                        <div className={`${bot.status}Bot`}></div>
+                    </div>
+                })}
+            </div>
+        </div>
+
+    )
     return (
-        <div className={styles.roomRoot}>{room?.name}</div>
+        <div className={styles.roomRoot}>
+            <Section title1={room?.name} title2={`Gérer la salle ${room?.id}`} contentChildrenNodes={[botsPanel()]}></Section>
+        </div>
     )
 }
