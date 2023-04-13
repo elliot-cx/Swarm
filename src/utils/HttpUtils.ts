04a2cca615324319546dedd1d2fb872238b97029
Gerard2par2 <83214983+Gerard2par2@users.noreply.github.com>
2023-04-13 22:05:05 +0200

    Removed require axios

--- src/utils/HttpUtils.ts
+++ src/utils/HttpUtils.ts
@@ -1,10 +1,7 @@
 export const API_URL = 'http://localhost:6969'
-
-const axios = require('axios');
-export namespace HttpUtils {   
-    export const fetchData = <T>(apiRoute:string,params?:{[key:string]:any}): Promise<T> => {
-        return fetch(`${API_URL}/${apiRoute}`,params ? params : {})
-        .then((response) => response.json())
+export namespace HttpUtils {
+    export const fetchData = <T>(apiRoute: string, params?: { [key: string]: any }): Promise<T> => {
+        return fetch(`${API_URL}/${apiRoute}`, params ? params : {})
+            .then((response) => response.json())
     }
-
 }
