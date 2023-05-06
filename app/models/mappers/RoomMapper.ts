5159e982272655be83caa25487f3b2cc390f3cc2
Gerard2par2 <83214983+Gerard2par2@users.noreply.github.com>
2023-05-06 12:18:44 +0200

    fixed broken brain

--- app/models/mappers/RoomMapper.ts
+++ app/models/mappers/RoomMapper.ts
@@ -1,15 +1,21 @@
 import { Room } from "../room";
 
 export namespace RoomMapper {
-    export const getDtoFromJKLMRoomResponse = (res: any): Room => {
-        return({
-            id: res.roomCode as string,
-            name: res.name as string,
-            type: res.gameId as string,
-            nbPlayers: res.playerCount as number,
-            isActive: false,
-            link:'',
-            bots: []
-        })
+    export const getDtoFromJKLMRoomResponse = (res: any): Room[] => {
+        const rooms: Room[] = [];
+        if(res.publicRooms){
+            res.publicRooms.forEach((room: any) => {
+                rooms.push({
+                    id: room.roomCode as string,
+                    name: room.name as string,
+                    type: room.gameId as string,
+                    nbPlayers: room.playerCount as number,
+                    isActive: false,
+                    link:'',
+                    bots: []
+                });
+            });
+        }
+        return rooms;
     }
 }
